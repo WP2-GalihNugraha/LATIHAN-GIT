0dacadfc2c27f55fc60c0cb494afa693ab4a79bf
WP2-GalihNugraha <90912805+WP2-GalihNugraha@users.noreply.github.com>
2021-09-19 05:00:56 +0700

    Add files via upload

---
+++ Contoh 1.php
@@ -0,0 +1,12 @@
+Contoh 1
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Galih Nugraha
+            Saya tinggal di Daerah Sentul
+            Hobi saya bermain Game Online";
+    }
+}
